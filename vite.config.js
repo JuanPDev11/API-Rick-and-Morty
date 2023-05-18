7bd4220d0ce29586ad67aa31c754eeb425e6bbcd
juan lucumi <lobbyscash@gmail.com>
2023-05-18 18:23:49 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://JuanPDev11.github.io/API-Rick-and-Morty/",
   plugins: [react()],
 })
